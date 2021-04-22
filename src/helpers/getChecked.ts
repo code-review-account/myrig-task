a94bd8738dd1e8dbe28e8022a5620d64ce8906e8
Vlad Niktin <v.nikitin@myrig.com>
2021-04-22 16:54:29 +0300

    Revert "my commit 1"
    This reverts commit 2824eb7d

--- src/helpers/getChecked.ts
+++
@@ -1,9 +0,0 @@
-import { UserType } from "../types/user-type"
-
-let getChecked = (users: UserType[]) => {
-    return !!users.find((user) => {
-        return user.checked === true
-    })
-}
-
-export default getChecked;
